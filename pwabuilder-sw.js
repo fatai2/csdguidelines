ab02a0fd1867ddc548d51752536b891740853dc0
fatai2 <31743893+fatai2@users.noreply.github.com>
2020-04-22 16:19:14 +0000

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,8 +1,8 @@
-//This is the service worker with the Advanced caching
+// This is the service worker with the Cache-first network
+
+const CACHE = "pwabuilder-precache";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-const CACHE = "pwabuilder-adv-cache";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -10,33 +10,9 @@ self.addEventListener("message", (event) => {
   }
 });
 
-const networkFirstPaths = [
-  /* Add an array of regex of paths that should go network first */
-  // Example: /\/api\/.*/
-  '/csdguidelines/index.html'
-];
-
-const networkOnlyPaths = [
-  /* Add an array of regex of paths that should always come from the network */
-  // Example: /\/api\/.*/
-]
-
-networkFirstPaths.forEach((path) => {
-  workbox.routing.registerRoute(
-    new RegExp(path),
-    new workbox.strategies.NetworkFirst({
-      cacheName: CACHE
-    })
-  );
-});
-
-networkOnlyPaths.forEach((path) => {
-  workbox.routing.registerRoute(
-    new RegExp(path),
-    new workbox.strategies.NetworkOnly({
-      cacheName: CACHE
-    })
-  );
-});
-
-
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: CACHE
+  })
+);
