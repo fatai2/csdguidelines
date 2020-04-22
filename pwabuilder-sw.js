cf25584bbc7e4614d4d6e92015c13c3c0cb59743
fatai2 <31743893+fatai2@users.noreply.github.com>
2020-04-22 16:24:14 +0000

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,4 @@
-// This is the service worker with the Cache-first network
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
 
-const CACHE = "pwabuilder-precache";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
-    cacheName: CACHE
-  })
-);
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
