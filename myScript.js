b166b32085170714a872dc4634ef1de9570bcf61
fatai2 <31743893+fatai2@users.noreply.github.com>
2020-11-04 21:51:59 +0000

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -25,3 +25,10 @@ function includeHTML() {
     }
   }
 };
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
